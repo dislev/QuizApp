6993cb424628a0f52ce361323c74c25d52c4cdc7
Ryuichi Itoi <t.link.full.contact@gmail.com>
2014-02-02 19:04:41 -0800

    question file for data

---
+++ questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        question:"Who is the 1st US President?",
+        choices:["Aberaham Lincoln",
+                "George Washington",
+                "Thomas Edison"],
+        correct:1
+    },
+    {
+        question:"Who discovered electricity?",
+        choices:["Aberaham Lincoln",
+                "George Washington",
+                "Thomas Edison"],
+        correct:2
+    },
+    {
+        question:"Who is known to not lie?",
+        choices:["Aberaham Lincoln",
+                "George Washington",
+                "Thomas Edison"],
+        correct:0
+    }
+    ];
